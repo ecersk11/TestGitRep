40eb74600c5267faa70b27dbe21816f4e02c3f3b
ecersk11 <ecersk11@users.noreply.github.com>
2015-05-21 13:25:26 +0530

    Updated Main

--- GitRepoTest2/src/testMain.java
+++ GitRepoTest2/src/testMain.java
@@ -7,7 +7,7 @@ public class testMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1");
-		System.out.println("test2");
+		System.out.println("test3");
 		//test fetch
 
 	}
